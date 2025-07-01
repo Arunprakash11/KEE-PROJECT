93013a0fdf4bde07d0986b27b5a9654331c55bd8
Arunprakash11 <prakasharumprakashm@gmail.com>
2025-07-01 16:53:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/*type="application/ld+json">
+{
+
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "@url": "https://yourwebsite.com",
+    "logo":
+"https://yourwebsite.com/Logo/KEE LOGO.png",
+    "name": "Kalyani E Enterprie"
+
+}*/
