c3e9c8ddc6dc893d5a7233b1e9cc9a54221b642b
Arunprakash11 <prakasharumprakashm@gmail.com>
2025-07-03 11:51:03 +0530

    New changes

--- script.js
+++ script.js
@@ -9,3 +9,26 @@
     "name": "Kalyani E Enterprie"
 
 }*/
+
+function showAlert() {
+
+    alert("Thank you for your interest! We'll share more info soon.");
+
+}
+
+function validateform() {
+
+    const name = document.getElementById("name").Value.trim();
+
+    const email = document.getElementById("email").Value.trim();
+    
+    const message = document.getElementById("message").Value.trim();
+
+    if (!name || !email || !message) {
+        alert("Please fill in all feilds.");
+        return false; // stop form submission
+    }
+
+    alert("Message Sent Successfully!");
+    return true;
+}
